6676fab1b10108fcbbc8eba9aab5c6cce21c36c3
Renata Cummins <renatacummins@gmail.com>
2019-06-18 18:28:38 -0400

    Refactor #update to show errors if unsuccessful

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,9 +20,12 @@ class UsersController < ApplicationController
 
     def update
         user = User.find(params[:id])
-        user.update(user_params)
-
-        render json: user
+        if user.update(user_params)
+            render json: user
+        else
+            render json: user.errors.full_messages,
+                status: :unprocessable_entity
+        end
     end
 
     def destroy
