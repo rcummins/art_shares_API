67f23a95f8e27ff3f38b0c05afc7221fb7ebe531
Renata Cummins <renatacummins@gmail.com>
2019-06-18 10:08:59 -0400

    Refactor UsersController#create so it can create users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,14 @@ class UsersController < ApplicationController
     end
 
     def create
-        render json: params
+        user = User.new(user_params)
+
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages,
+                status: :unprocessable_entity
+        end
     end
 
     def show
@@ -13,5 +20,11 @@ class UsersController < ApplicationController
 
     def update
         render json: params
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email)
     end
 end
