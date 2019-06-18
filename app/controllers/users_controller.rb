ddc582ea78f6061b24099f32e678a1f3d36e2b9c
Renata Cummins <renatacummins@gmail.com>
2019-06-18 10:32:40 -0400

    Refactor UsersController#show so it shows the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,9 @@ class UsersController < ApplicationController
     end
 
     def show
-        render json: params
+        user = User.find(params[:id])
+
+        render json: user
     end
 
     def update
