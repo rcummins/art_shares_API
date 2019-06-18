fa6223a99dc977f179f1a6a8b1ea6259ae710c4c
Renata Cummins <renatacummins@gmail.com>
2019-06-18 16:29:09 -0400

    Add associations between User and Artwork through ArtworkShares

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
+
+    has_many :artwork_views, class_name: :ArtworkShare, foreign_key: :viewer_id
+    has_many :viewed_artworks, through: :artwork_views, source: :artwork
 end
