b908e162cfcf70c4e6194054f2c3abaa0b2e7acb
Renata Cummins <renatacummins@gmail.com>
2019-06-18 17:15:48 -0400

    Add database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create([
+    {username: 'Renata'},
+    {username: 'Jen'},
+    {username: 'Skyler'},
+    {username: 'Lance'}
+])
+
+Artwork.create([
+    {title: 'Rainbow', 
+        image_url: 'https://en.wikipedia.org/wiki/Rainbow#/media/File:Double-alaskan-rainbow.jpg',
+        artist_id: 1},
+    {title: 'Woman with a Parasol',
+        image_url: 'https://en.wikipedia.org/wiki/Claude_Monet#/media/File:Claude_Monet_-_Woman_with_a_Parasol_-_Madame_Monet_and_Her_Son_-_Google_Art_Project.jpg',
+        artist_id: 1},
+    { title: 'The Scream',
+        image_url: 'https://en.wikipedia.org/wiki/The_Scream#/media/File:Edvard_Munch,_1893,_The_Scream,_oil,_tempera_and_pastel_on_cardboard,_91_x_73_cm,_National_Gallery_of_Norway.jpg',
+        artist_id: 2},
+    
+])
+
+ArtworkShare.create([
+    {artwork_id: 1, viewer_id: 2},
+    {artwork_id: 1, viewer_id: 3},
+    {artwork_id: 1, viewer_id: 4},
+    {artwork_id: 2, viewer_id: 2},
+    {artwork_id: 3, viewer_id: 1},
+    {artwork_id: 3, viewer_id: 3},
+    {artwork_id: 3, viewer_id: 4},
+])
