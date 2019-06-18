db1b3f44dcfa5529a48121ffd8f0b3385ef9c7ad
Renata Cummins <renatacummins@gmail.com>
2019-06-18 09:58:39 -0400

    Add basic routes, controller, model for users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+    def index
+        render json: User.all
+    end
+
+    def create
+        render json: params
+    end
+
+    def show
+        render json: params
+    end
+
+    def update
+        render json: params
+    end
+end
