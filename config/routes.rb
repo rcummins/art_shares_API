4d20aec80e1783949d85df2c336b2dcebcf92a02
Renata Cummins <renatacummins@gmail.com>
2019-06-19 12:07:13 -0400

    Refactor to avoid making duplicate users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index, :create, :show, :update, :destroy]
-
-  resources :users do
+  resources :users, only: [:index, :create, :show, :update, :destroy] do
     resources :artworks, only: [:index]
   end
 
